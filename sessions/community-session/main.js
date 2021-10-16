4312fb2ca0e1b193ba18c449852e4aaf877e304d
German <german.mtz@protonmail.com>
2021-10-16 13:21:10 -0500

    add solutions to community sessions exercises

--- sessions/community-session/main.js
+++ sessions/community-session/main.js
@@ -1,1 +1,274 @@
 console.log("Community Session Javascript")
+
+const bookList = [
+    {
+        name : "The Lightning Thief",
+        author : "Rick Riordan",
+        genre_s : "fantasy",
+        inStock : true,
+        pages : 384,
+        ratingCount: 5,
+        price: {
+            amount:9.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"2019-10-15",
+        tags :[
+            "Juvenile Fiction",
+        ]
+    },
+    {
+        name : "Old Possum's Book of Practical Cats",
+        author : "T. S. Eliot",
+        genre_s : "fantasy",
+        inStock : true,
+        pages : 384,
+        ratingCount: 3,
+        price: {
+            amount:9.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"2019-10-15",
+        tags :[
+            "Juvenile Fiction",
+        ]
+    },
+    {
+        name : "The Everything Book of Cats and Kittens",
+        author : "DK",
+        genre_s : "Suspense",
+        inStock : true,
+        pages : 120,
+        ratingCount: 2,
+        price: {
+            amount:5.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"2018-06-12",
+        tags :[
+            "Best Seller","Popular"
+        ]
+    },
+    {
+        name : "There are Cats in this Book",
+        author : "Viviane Schwarz",
+        genre_s : "Didactic",
+        inStock : true,
+        pages : 220,
+        ratingCount: 4,
+        price: {
+            amount:17.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"2008-06-12",
+        tags :[
+            "Cats","Animals"
+        ]
+    },
+    {
+        name : "Don Quijote",
+        author : "Miguel de Cervantes",
+        genre_s : "Literature",
+        inStock : false,
+        pages : 520,
+        ratingCount: 7,
+        price: {
+            amount:47.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"1990-06-12",
+        tags :[
+            "Best Seller","Reference"
+        ]
+    },
+    {
+        name : "The lord of the rings",
+        author : "J.R.R. Tolkien",
+        genre_s : "fantasy",
+        inStock : false,
+        pages : 620,
+        ratingCount: 9,
+        price: {
+            amount:59.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"2001-06-12",
+        tags :[
+            "Swords","Golum"
+        ]
+    },
+    {
+        name : "Harry Potter and the Philosopher's Stone",
+        author : "J.K. Rowling.",
+        genre_s : "fantasy",
+        inStock : true,
+        pages : 820,
+        ratingCount: 10,
+        price: {
+            amount:99.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"2006-06-12",
+        tags :[
+            "Magician","Harry Potter"
+        ]
+    },
+    {
+        name : "The Hobbit",
+        author : "J.R.R. Tolkien.",
+        genre_s : "fantasy",
+        inStock : true,
+        pages : 1220,
+        ratingCount: 8,
+        price: {
+            amount:69.99,
+            currencyCode:"USD"
+        },
+        publishedDate:"2006-06-12",
+        tags :[
+            "Golum","Dragons","Best Seller"
+        ]
+    },
+];
+
+
+// Fucnion declarativa 
+
+[1,2].forEach(() => {});
+
+function name(params) {
+    return
+};
+
+const numbers = [1, 2,4];
+
+
+const getResult = (array, callback) => {
+    console.log(array)
+};
+
+const promediar = () => {
+console.log('funcion prodemdiar')
+};
+
+getResult(numbers, promediar )
+
+
+// Funcion de expresion 
+
+// Obtener todos los tags que existen
+// Ordenar los libros en orden alfabetico
+// realizar filtrado dinamico por genero,
+// realizar busqueda por titulo, encontrar coincidencias
+// obtener top 3 de los libros mas rankeados
+// obtener los 3 libros mas baratos
+// obtener el libro con fecha de publicacion mas reciente,
+// obtener los libros fuera de stock
+
+
+
+// Obtener todos los tags que existen
+
+//Crear una function 
+//Iterar sobre el array de bookList
+//Buscar la propiedad tag dentro del objeto en turno
+//Ir a la propiedad tag del libro en turno
+//Iterar la propiedad tag
+//Ir tag por tag en la propiedad.
+//Crear variable para guardar los tags
+//Si el tag no se encuentra en la variable a devolver, insertar
+// el tag en la variable a devolver
+// devolver la variable
+
+const getBookTags = () => {
+    const tags = []
+        bookList.forEach((book) => {
+            book.tags.forEach((tag)=>{
+                if (!tags.includes(tag))
+                tags.push(tag)
+            })
+        })
+    return tags
+}
+
+// Ordenar los libros en orden alfabetico tomando el nombre
+
+/* 
+Crear funcion
+Iterar el arreglo de libros
+Tomar el nombre de cada libro
+Usar el metodo sort para ordenarlo
+Retonar el arreglo ordenado
+*/
+
+const sortBooksByName = () => {
+ const sortedBooks =  bookList.sort((book1, book2) => {
+        a = book1.name.toUpperCase(),
+        b = book2.name.toUpperCase();
+        return a == b ? 0 : a > b ? 1 : -1;
+    });
+
+    return sortedBooks
+}
+
+// realizar filtrado dinamico por genero,
+
+//Return [{},{},{}]
+
+//Escribir funcion que reciba el genero como parametro
+//Usar filter. reduce para iterar el arreglo
+//Retornar el arreglo filtrado
+
+const getBooksByGenre = (genreToFilter) => {
+    return bookList.filter((book) => {
+        return book.genre_s === genreToFilter
+    })
+}
+
+// realizar busqueda por titulo, encontrar coincidencias
+
+/* 
+
+Generar la function que escriba parametro
+el nombre a buscar
+Crear un arreglo varia donde guardemos los libros con 
+coincidencias
+Usar un filter usando de condiciones una expresion regular 
+o usando includes
+Retornar el arreglo con los libros con coincidencias
+
+*/
+
+//Includes solution
+
+const searchForTitles = (wordToFilter) => {
+    return bookList.filter((book) => {
+        return book.name.includes(wordToFilter)
+    })
+}
+
+//Regex Solution
+
+const searchForTitlesRegex = (wordToFilter) => {
+    return bookList.filter((book) => {
+        const regex = new RegExp(wordToFilter, 'g')
+        return book.name.match(regex)
+    })
+}
+
+// obtener top 3 de los libros mas rankeados
+const getTop3RankedBooks = () => {
+
+}
+// obtener los 3 libros mas baratos
+const getTop3CheapestBooks = () => {
+
+}
+// obtener el libro con fecha de publicacion mas reciente
+const getMostRecentBook = () => {
+
+}
+// obtener los libros fuera de stock
+const getOutOfStockBook = () => {
+
+}
